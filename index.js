41b279575e10220f6f7ef1c84d632d624b10f791
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2021-02-03 22:51:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,70 @@
 const puppeteer = require('puppeteer');
 const Sheet = require('./sheet');
 
-const url = 'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/';
-
 (async function () {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
 
     const sheet = new Sheet();
     await sheet.load();
-    // create sheet with title
-    const title = await page.$eval('.title a', el => el.textContent);
-    const sheetIndex = await sheet.addSheet(title.slice(0, 99), ['points', 'text']); 
 
-    // expand all comment threads
-    let expandButtons = await page.$$('.morecomments');
-    while (expandButtons.length) {
-        for (let button of expandButtons) {
-            await button.click();
-            await page.waitForTimeout(500);
-        }
-        await page.waitForTimeout(1000);
-        expandButtons = await page.$$('.morecomments');
+    const sheetNames = [];
+    for (let i = 1; i < sheet.doc.sheetCount; i++) {
+        sheetNames.push(sheet.doc.sheetsByIndex[i].title);
     }
 
-    // select all comments, scrape text and points
-    const comments = await page.$$('.entry');
-    const formattedComments = [];
-    for (let comment of comments) {
-        // scrape points
-        const points = await comment.$eval('.score', el => el.textContent).catch(err => console.error('no score'));
-        // scrape text
-        const rawText = await comment.$eval('.usertext-body', el => el.textContent).catch(err => console.error('no text'));
-        if (points && rawText) {
+    const urls = await sheet.getRows(0);
+    for (let url of urls) {
+        await page.goto(url.url);
 
-            const text = rawText.replace(/\n/g, '');
-            formattedComments.push({ points, text });
+        // create sheet with title
+        const title = await page.$eval('.title a', el => el.textContent.slice(0, 99));
+
+        // remove duplicate sheet titles
+        for (let name of sheetNames) {
+            if (name === title) {
+                await sheet.doc.sheetsByTitle[name].delete();
+            }
         }
+
+        const sheetIndex = await sheet.addSheet(title, ['points', 'text']);
+
+        // expand all comment threads
+        let expandButtons = await page.$$('.morecomments');
+        while (expandButtons.length) {
+            for (let button of expandButtons) {
+                await button.click();
+                await page.waitForTimeout(500);
+            }
+            await page.waitForTimeout(1000);
+            expandButtons = await page.$$('.morecomments');
+        }
+
+        // select all comments, scrape text and points
+        const comments = await page.$$('.entry');
+        const formattedComments = [];
+        for (let comment of comments) {
+            // scrape points
+            const points = await comment.$eval('.score', el => el.textContent).catch(err => console.error('no score'));
+            // scrape text
+            const rawText = await comment.$eval('.usertext-body', el => el.textContent).catch(err => console.error('no text'));
+            if (points && rawText) {
+                // remove \n line breaks with regEx
+                const text = rawText.replace(/\n/g, '');
+                formattedComments.push({ points, text });
+            }
+        }
+
+        // sort comments by points
+        formattedComments.sort((a, b) => {
+            const pointsA = Number(a.points.split(' ')[0]);
+            const pointsB = Number(b.points.split(' ')[0]);
+            return pointsB - pointsA;
+        })
+
+        // insert into google spreadsheet
+        sheet.addRows(formattedComments, sheetIndex);
     }
-
-    // sort comments by points
-
-    formattedComments.sort((a, b) => {
-        const pointsA = Number(a.points.split(' ')[0]);
-        const pointsB = Number(b.points.split(' ')[0]);
-        return pointsB - pointsA;
-    })
-
-    // insert into google spreadsheet
-    sheet.addRows(formattedComments, sheetIndex);
 
     await browser.close();
 })()
